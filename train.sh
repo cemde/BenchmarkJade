caf384bca1f162bada58c87bbdf8ebd931884d8b
cemde <c.emde@me.com>
2024-02-08 19:33:11 +0000

    added slurm scripts

---
+++ train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=00-00:30:00
+#SBATCH --job-name=inference
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:8
+#SBATCH --partition=big
+#SBATCH --output ./logs/slurm/slurm-%j-%x.out # STDOUT
+
+# define variables
+PYTHON="/jmain02/home/J2AD009/ttl04/cxe09-ttl04/anaconda3/envs/advcal/bin/python"
+HOME_DIR="/jmain02/home/J2AD009/ttl04/cxe09-ttl04/BenchmarkJade"
+DATASET="imagenet"
+
+module purge
+
+$PYTHON ${HOME_DIR}/train.py --dataset ${DATASET}
